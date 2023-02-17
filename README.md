98f01f3e7ba3468cf17a32731083a0ee8d5289cf
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-17 18:01:52 +0530

    Details

--- README.md
+++ README.md
@@ -14,9 +14,14 @@ About the game: When you load this in the browser, which will look like this -
 
 A random number gets generated between 1 to 20, the user has to enter a number between 1 to 20. The score is set to 20 in the beginning, after each attempt the score reduce by 1, till the user guess the right random number. This is a basic walkthorugh now let's see the condtions
 
-> **Condtion:**
+> ### **Condtion:**
 
+>  Condtion 1 - The range must be between 1 to 20, else it will throw an error
 
--hih
+> Condition 2 - You will get 20 chances to guess the right number else you will lose the game and it will throw this error
+
+> Condition 3 - It must be an integer number
+
+![_8_lost](imgs/_8_lost.png)
 
 
--- script.js
+++ script.js
@@ -13,31 +13,37 @@ const setQuerySelectorValue = (id,value) => {
 const setBackgroundColor = (value) => {
     document.querySelector('body').style.backgroundColor = value;
 }
-const getValue =  () => {
-    //String to number
-    guessValue = Number(document.querySelector('.guess').value);
-    console.log(guessValue);
-    setBackgroundColor('#222');
 
-    //If score value is positive
-    //Condition 1: Guess value is not null
-    if (!guessValue) {
-        console.log("No number");
-        setQuerySelectorValue('.message','No number😒')
-    }
-        //Condition 2: Guess value is in given range
-    else if (guessValue < 1 || guessValue > 20) {
+const againButton = () => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    scoreValue = 20;
+    flag = false;
+
+    document.querySelector('.guess').value = ''; //textContent does not works use value
+
+    setQuerySelectorValue('.message','Start guessing😁...');
+    setQuerySelectorValue('.score', scoreValue);
+    setQuerySelectorValue('.number','?');
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    console.clear();
+    console.log(`Secret Number: ${secretNumber}`);
+}
+
+const checkCondition = (numValue) => {
+
+    //Condition 3: Guess value is in given range
+    if (numValue < 1 || numValue > 20) {
         console.log("Out of Range!");
         setQuerySelectorValue('.message','Out of Range 😑');
         setBackgroundColor('#a017d2');
     }
         
-    else if (guessValue != secretNumber) {
+    else if (numValue != secretNumber) {
         if (scoreValue > 1) {
-            console.log("Too High");
-            //Condition 3: Guess value exceeds score value   
-            //Condition 4: Guess value less then score value
-            setQuerySelectorValue('.message', (guessValue < secretNumber) ? 'Too Low 😪' : 'Too High! 😲');
+            //Condition 4: Guess value exceeds score value OR Guess value less then score value
+            setQuerySelectorValue('.message', (numValue < secretNumber) ? 'Too Low 😪' : 'Too High! 😲');
         }
         else {
             console.log("You Lose");
@@ -48,37 +54,50 @@ const getValue =  () => {
             console.log("Check");
         }   
     }
-    //Condition 5: Guess value matches!
-    else if (guessValue === secretNumber) {
+    //Condition 6: Guess value matches!
+    else if (numValue === secretNumber) {
         flag = true;
         console.log("Won");
         setQuerySelectorValue('.message', 'Congratulations🤩🥳')
-        setQuerySelectorValue('.number',guessValue);
+        setQuerySelectorValue('.number',numValue);
         setBackgroundColor('#60b347');
         document.querySelector('.number').style.width = '30rem';
         highscore = (scoreValue > highscore) ? scoreValue : highscore;
         setQuerySelectorValue('.highscore',highscore);
     }
+}
+
+const getValue = () => {
+
+    setBackgroundColor('#222');
+
+    //String to number
+    guessValue = Number(document.querySelector('.guess').value);
+    console.log(guessValue);
+
+    //If score value is positive
+    //Condition 1: Guess value is not null
+    if (!guessValue) {
+        console.log("No number");
+        setQuerySelectorValue('.message', 'No number😒');
+        setBackgroundColor('#4c49ec');   
+    }
+    //Condition 2: Guess value is decimal
+    else if (!Number.isInteger(guessValue)) {
+        console.log("Not a integer value");
+        setQuerySelectorValue('.message', 'Decimal not allowed🥸')
+        //setQuerySelectorValue('.message', 'Decimal input not allowed');
+        setBackgroundColor('#626464');
+        setTimeout(againButton,3000);
+    }
+    else {
+        checkCondition(guessValue)
+    }        
     scoreValue = (flag && scoreValue > 0) ? scoreValue : --scoreValue;
     setQuerySelectorValue('.score', scoreValue);
     }
 
-const againButton = () => {
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    scoreValue = 20;
-    flag = false;
 
-    document.querySelector('.guess').value = ''; //textContent does not works use value
-
-    setQuerySelectorValue('.message','Start guessing...');
-    setQuerySelectorValue('.score', scoreValue);
-    setQuerySelectorValue('.number','?');
-
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    console.clear();
-    console.log(`Secret Number: ${secretNumber}`);
-}
 document.querySelector('.check').addEventListener('click', getValue);
 document.querySelector('.again').addEventListener('click', againButton);
 
